c301b7aa005af326970bc4e2acb93f7bfbdc401b
gordo <rogelioalbertopedraza@gmail.com>
2022-03-17 13:23:12 -0500

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,60 @@
-window.alert ("this is an alert! js is running!");
+var playerMoney = 10;
+
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+console.log(playerName, playerHealth, playerAttack);
+
+var enemyName = "Bender Rodriguez";
+var enemyHealth = 100;
+var enemyAttack = 10;
+
+console.log(enemyName, enemyHealth, enemyAttack);
+
+var fight = function(){
+    window.alert("Welcome to Robot Gladiators!");
+
+    var promptFight =  window.prompt("Would you like to FIGHT or SKIP this battle?");
+
+    if (promptFight === "fight" || promptFight === "FIGHT") {
+        enemyHealth = enemyHealth - playerAttack;
+
+        console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+
+        if (enemyHealth <= 0){
+            window.alert(enemyName + " has died!");
+        }
+        else {
+            window.alert(enemyName + " still has " + enemyHealth + " health left.");
+        }
+
+        playerHealth = playerHealth - enemyAttack;
+
+        console.log(enemyName + " attacked " + playerName +". " + playerName + " now has " + playerHealth + " health remaining.");
+
+        if (playerHealth <= 0){
+            window.alert(playerName + " has died!");
+        }
+        else {
+            window.alert(playerName + " still has " + playerHealth + " health left.");
+        }
+    }
+    else if (promptFight === "skip" || promptFight === "SKIP") {
+
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+        if (confirmSkip){
+           window.alert(playerName + " has chosen to skip the fight!");
+           playerMoney = playerMoney - 2; 
+        }
+        else {
+            fight();
+        }
+    }
+    else {
+        window.alert("You must choose a valid option! Try again."); 
+    }    
+};
+
+fight();
